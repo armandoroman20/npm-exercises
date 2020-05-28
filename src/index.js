d2d37281535f3d844b9e22ed0e216c1577e7fe52
Armando Roman <armando.roman.jr20@gmail.com>
2020-05-28 08:49:39 -0500

    exercies for npm complete

--- src/index.js
+++ src/index.js
@@ -9,5 +9,20 @@ const sayHello = name => {
 sayHello('armando');
 
 $('body').css({
-    'background-color': 'purple'
+    'background-color': 'yellow'
 });
+
+//===== import method=====//
+
+// import {sayHi, hello, bye} from './say-hello';
+//
+// console.log(bye);
+// console.log(hello);
+// console.log(sayHi());
+
+//=====require method=====//
+
+const helloWorld = require('./hello-world');
+console.log(helloWorld.hello);
+console.log(helloWorld.bye);
+console.log(helloWorld.sayHi());
---
+++ src/say-hello.js
@@ -0,0 +1,15 @@
+"use strict";
+
+//=====IMPORT METHOD=====//
+//
+// export const sayHi = () => 'hi';
+// export const hello = 'Hello World';
+// export const bye = "Bye World";
+
+//=====REQUIRE METHOD=====//
+
+const hello = 'Hello World';
+const bye = "Bye World";
+const sayHi = () => 'hi';
+
+module.exports = {hello, bye, sayHi};
